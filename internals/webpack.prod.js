714d64184d9a7fde690129d9a3e6819dcc93f80d
Dan Balaban <dbalaban@lampix.co>
2018-12-07 14:31:39 +0200

    Added .otf font extensions

--- internals/webpack.prod.js
+++ internals/webpack.prod.js
@@ -37,7 +37,7 @@ module.exports = () => ({
         }
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
         loader: 'file-loader?name=[name].[ext]'
       },
       {
